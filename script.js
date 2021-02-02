05adc3fe5e3044a1665d5bb5b4eb706df47cddac
bhansen0091 <bhansen0091@yahoo.com>
2021-02-02 15:27:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -287,6 +287,23 @@ function swapTowardCenter(arr) {
 console.log(swapTowardCenter(testArr));
 console.log(swapTowardCenter(testArr2));
 
+
+
+var testArr = [true,42,"Ada",2,"pizza"];
+
+function swapCenter(arr) {
+    for (var i = 0; i < arr.length / 2; i+=2) {
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
+
+console.log(swapCenter(testArr));
+
+//destructuring
+
 */
 
 //--------------------------------------------
